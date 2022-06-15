468c994bebd76d4f7ee7b10f79d9bf7ce79fe981
AntonArk <anton.ark80@gmail.com>
2022-06-15 17:18:15 +0400

    Update visit view

--- app.rb
+++ app.rb
@@ -11,7 +11,28 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
-	@barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+   
+
+   erb "<h2>Спасибо, Вы записаны!</h2>"
+
 end
